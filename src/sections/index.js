7fcff39c0ee5d0f69dded02b43e5ded094e48ecc
AnujHegishte <56145309+AnujHegishte@users.noreply.github.com>
2024-03-15 18:26:28 +0530

    File uploads

---
+++ src/sections/index.js
@@ -0,0 +1,20 @@
+import Hero from './Hero';
+import PopularProducts from './PopularProducts';
+import Services from './Services';
+import CustomerReviews from './CustomerReviews';
+import Footer from './Footer';
+import SpecialOffer from './SpecialOffer';
+import Subscribe from './Subscribe';
+import SuperQuality from './SuperQuality';
+
+
+export {
+    Hero,
+    PopularProducts,
+    Services,
+    CustomerReviews,
+    Footer,
+    SpecialOffer,
+    Subscribe,
+    SuperQuality,
+};
